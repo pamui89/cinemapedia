7ff8293c2032f958f2121e16d49edc71d80c41f1
Pamui89 <pablo.m@ur-fixer.com>
2024-01-11 09:03:43 +0200

    Renaming

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-  static String theMoviedbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No API key';
+  static String theMovieDbKey = dotenv.env['THE_MOVIE_DB_KEY'] ?? 'No API key';
 }
