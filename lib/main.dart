1e155d39bc9a70548ba4e600144b0aaed406d132
Pamui89 <pablo.m@ur-fixer.com>
2024-01-11 07:17:11 +0200

    Changes on main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
