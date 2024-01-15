a1e368a7f384fd57ce1dc499eb7f7e6d29effa37
Pamui89 <pablo.m@ur-fixer.com>
2024-01-15 07:33:07 +0200

    Movie datasource implemented

--- lib/infraestructure/datasources/the_moviedb_datasource.dart
+++ lib/infraestructure/datasources/the_moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/the_movie_db/the_movie_db_response.dart';
 import 'package:dio/dio.dart';
 
 const theMovieDbApiBaseUrl = 'https://api.themoviedb.org';
@@ -8,16 +10,25 @@ const theMovieDbApiVersion = '3';
 const theMovieDbApiBaseUrlWithVersion =
     '$theMovieDbApiBaseUrl/$theMovieDbApiVersion/';
 
-class TheMovieDbDatasource extends MoviesDataSource {
+class TheMovieDbDatasource extends MoviesDatasource {
   final dio = Dio(
-      BaseOptions(baseUrl: theMovieDbApiBaseUrlWithVersion, queryParameters: {
-    'api_key': Environment.theMovieDbKey,
-    'language': 'es-ES',
-  }));
+      BaseOptions(
+        baseUrl: theMovieDbApiBaseUrlWithVersion, 
+        queryParameters: {
+          'api_key': Environment.theMovieDbKey,
+          'language': 'es-ES',
+        }
+      )
+  );
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = dio.get('movie/now_playing');
-    final List<Movie> movies = [];
+    final response = await dio.get('movie/now_playing');
+    final theMovieDbresponse = TheMovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = theMovieDbresponse.results
+      .where((theMovieDb) => theMovieDb.posterPath != 'no-poster')
+      .map(
+      (themoviedb) => MovieMapper.theMovieDbToEntity(themoviedb)
+      ).toList();
 
     return movies;
   }
