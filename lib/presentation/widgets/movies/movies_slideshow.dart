13a1501137ff8f656d71cb31075f1fa1baae44b4
Pamui89 <pablo.m@ur-fixer.com>
2024-01-19 06:53:18 +0200

    Terminar el carrusel de películas

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -12,11 +12,11 @@ class MoviesSlideshow extends StatelessWidget {
       height: 210,
       width: double.infinity,
       child: Swiper(
-        viewportFraction: 0.8,
-        scale: 0.9,
-        autoplay: true,
-        itemCount: movies.length,
-        itemBuilder: (context, index) => _Slide(movie: movies[index])),
+          viewportFraction: 0.8,
+          scale: 0.9,
+          autoplay: true,
+          itemCount: movies.length,
+          itemBuilder: (context, index) => _Slide(movie: movies[index])),
     );
   }
 }
@@ -28,6 +28,28 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration =
+        BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
+      BoxShadow(color: Colors.black45, blurRadius: 10, offset: Offset(0, 10))
+    ]);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.backdropPath,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black12));
+                }
+                return FadeIn(child: child);
+              },
+            )),
+      ),
+    );
   }
 }
