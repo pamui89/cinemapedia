8c73ac13732db7d5e6ab65ba12a27e3c73e5810a
Pamui89 <pablo.m@ur-fixer.com>
2024-02-06 14:25:17 +0200

    196 - HumanFormats - Números cortos

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -90,14 +91,17 @@ class _Slide extends StatelessWidget {
             ),
           ),
           //* Rating
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800,),
-              const SizedBox(width: 3,),
-              Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color:Colors.yellow.shade800),),
-              const SizedBox(width: 3,),
-              Text('${movie.popularity}', style: textStyles.bodySmall,)
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800,),
+                const SizedBox(width: 3,),
+                Text('${movie.voteAverage}', style: textStyles.bodyMedium?.copyWith(color:Colors.yellow.shade800),),
+                const Spacer(),
+                Text(HumanFormats.number(movie.popularity), style: textStyles.bodySmall,)
+              ],
+            ),
           )
         ],
       ),
