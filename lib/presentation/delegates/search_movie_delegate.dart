4a87f3ee7f5c786d3601f1cec4d7319250e8184c
Pamui89 <pablo.m@ur-fixer.com>
2024-02-26 11:47:33 +0000

    228 - Leading Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,29 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar película';
-  
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text('buildActions'),
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          onPressed: () => query = '', 
+          icon: const Icon(Icons.clear)
+        ),
+      )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    const Text('buildLeading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
