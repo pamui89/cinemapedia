ea80943ac572d171dabd0c79a6a7a08ce35f6fc6
Pamui89 <pablo.m@ur-fixer.com>
2024-02-26 16:45:20 +0000

    232 - Mostrar calificación de la película

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -68,7 +69,7 @@ class _MovieSearchItem extends StatelessWidget {
           SizedBox(
             width: size.width *0.2,
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(10),
               child: Image.network(
                 movie.posterPath,
                 loadingBuilder: (context, child, loadingProgress) => FadeIn(child: child),
@@ -85,6 +86,16 @@ class _MovieSearchItem extends StatelessWidget {
                 (movie.overview.length>100)
                   ? Text('${movie.overview.substring(0,100)}...', style: textStyles.bodyMedium,)
                   : Text(movie.overview, style: textStyles.bodyMedium,),
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                    const SizedBox(width: 5,),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1).toString(),
+                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+                    )
+                  ],
+                )
               ],
             ),
           )
