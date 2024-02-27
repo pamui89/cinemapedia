619719d602184463ad74dfc5d601b8a238d34a70
Pamui89 <pablo.m@ur-fixer.com>
2024-02-27 07:51:10 +0000

    237 - Mantener un estado con las películas buscadas

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -26,17 +26,15 @@ class CustomAppbar extends ConsumerWidget {
           const Spacer(),
           IconButton(
               onPressed: () {
-                final movieRepository = ref.read(movieRepositoryProvider);
+                // TODO 
+                // final searchedMovies = ref.read(searchedMoviesProvider);
                 final searchQuery = ref.read(searchQueryProvider);
                 showSearch<Movie?>(
                     query: searchQuery,
                     context: context,
-                    delegate: SearchMovieDelegate(searchMovies: (query) {
-                      ref
-                          .read(searchQueryProvider.notifier)
-                          .update((state) => query);
-                      return movieRepository.searchMovies(query);
-                    })).then((movie) {
+                    delegate: SearchMovieDelegate(
+                      searchMovies: ref.read(searchedMoviesProvider.notifier).searchMoviesByQuery
+                    )).then((movie) {
                   if (movie == null) return;
                   context.push('/movie/${movie.id}');
                 });
