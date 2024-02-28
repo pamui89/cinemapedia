d2643e0c53246ffcda3391a307e84ec64448617f
Pamui89 <pablo.m@ur-fixer.com>
2024-02-28 13:43:50 +0000

    247 - ShellRoute - GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,23 +3,38 @@ import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/', 
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(childView: FavoritesView(),),
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? '0';
+                  return MovieScreen(movieId: int.parse(movieId));
+                },
+              ),
+            ]),
+        GoRoute(
+          path: '/favorites',
           builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? '0';
-            return MovieScreen(movieId: int.parse(movieId));
+            return const FavoritesView();
           },
         ),
-      ]
-    ),
-  ]
-);
+        GoRoute(
+          path: '/categories',
+          builder: (context, state) {
+            return const CategoriesView();
+          },
+        ),
+      ])
+]);
