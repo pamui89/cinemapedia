7c0c07212e77c3f5c33991debd62152bbcd23975
Pamui89 <pablo.m@ur-fixer.com>
2024-02-28 18:07:44 +0000

    252 - Configuración del Router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,18 +2,23 @@ import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
   GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = state.pathParameters['page'] ?? '0';
+        final pageIndexToInt = int.parse(pageIndex);
+        return HomeScreen(pageIndex: pageIndexToInt);
+      },
       routes: [
         GoRoute(
           path: 'movie/:id',
           name: MovieScreen.name,
           builder: (context, state) {
             final movieId = state.pathParameters['id'] ?? '0';
-            return MovieScreen(movieId: int.parse(movieId));
+            final movieIdToInt = int.parse(movieId);
+            return MovieScreen(movieId: movieIdToInt);
           },
         ),
       ])
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -4,13 +4,23 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    CategoriesView(),
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        bottomNavigationBar: CustomBottonNavigation(), body: FavoritesView());
+    return Scaffold(
+        bottomNavigationBar: const CustomBottonNavigation(),
+        body: IndexedStack(
+          index: pageIndex,
+          children: viewRoutes,
+        ));
   }
 }
-
