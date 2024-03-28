a865ebbbcf786699600ad7dde3fcc63931eb5666
Pamui89 <pablo.m@ur-fixer.com>
2024-03-28 09:21:28 +0000

    270 - Mostrar películas favoritas

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,15 +1,42 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+import '../../widgets/widgets.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullscreenLoader();
+
+    final favoriteMovies = ref.watch(favoriteMoviesProvider);
+          final List<Movie> favoriteMoviesList = favoriteMovies.values.toList();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            // title: Text('Movie name'),
+            title: Text(favoriteMoviesList[index].title),
+          );
+        },
       ),
-      body: const Center(child: Text('Favoritos')),
     );
   }
 }
