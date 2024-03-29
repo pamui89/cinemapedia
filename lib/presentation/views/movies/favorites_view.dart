3e2f19cfc7e52ac880ce807b77b745f60beb9a2d
Pamui89 <pablo.m@ur-fixer.com>
2024-03-29 12:21:36 +0000

    276 - Mostrar mensajes si no hay favoritos

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/presentation/providers/storage/favorite_movies_provi
 import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -18,7 +19,6 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   void initState() {
     super.initState();
-
 
     loadNextPage();
   }
@@ -36,11 +36,37 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
 
   @override
   Widget build(BuildContext context) {
-    // final initialLoading = ref.watch(initialLoadingProvider);
-    // if (initialLoading) return const FullscreenLoader();
-
     final List<Movie> favoriteMovies =
         ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite_outline_sharp,
+            size: 60,
+            color: colors.primary,
+          ),
+          Text(
+            '¡Oh, no!',
+            style: TextStyle(fontSize: 30, color: colors.primary),
+          ),
+          const Text(
+            'No tienes películas favoritas',
+            style: TextStyle(fontSize: 20, color: Colors.black45),
+          ),
+          const SizedBox(height: 20,),
+          FilledButton.tonal(
+            onPressed: () => context.go('/home/0'), 
+            child: const Text('Empieza a buscar')
+          )
+        ],
+      ));
+    }
     return Scaffold(
         body: MovieMasonry(loadNextPage: loadNextPage, movies: favoriteMovies));
   }
